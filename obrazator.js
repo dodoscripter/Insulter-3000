724b3a093f17534b3889e005c82b894e697684ff
IgorOczk <lolwatt221@gmail.com>
2016-12-31 23:28:30 +0100

    Update obrazator.js

--- obrazator.js
+++ obrazator.js
@@ -1,8 +1,8 @@
-var losoweCzesciCiala = ["Twarz", "Noga", "Ręka", "Miednica"];
-var losowePrzymiotniki = ["Cuchnąca", "Brudna", "Głupia", "Brzydka", "Okropna"];
-var losoweSlowa = ["Mucha", "Glista", "Małpa", "Jaszczurka", "Kawa", "Odbytnica"]
+var losoweCzesciCiala = ["Face", "Leg", "Hand", "Butt"];
+var losowePrzymiotniki = ["Stinky", "Dirty", "stupid", "ugly", "Terrible"];
+var losoweSlowa = ["fly", "Warm", "Monkey", "Lizard", "Coffe", "Mud"]
 var losowaCzescCiala = losoweCzesciCiala[Math.floor(Math.random() * 4)];
 var losowyPrzymiotnik = losowePrzymiotniki[Math.floor(Math.random() * 5)];
 var losoweSlowo = losoweSlowa[Math.floor(Math.random() * 6)];
-var zdanie = "Twoja " + losowaCzescCiala + " jest jak " + losowyPrzymiotnik + " "  + losoweSlowo;
+var zdanie = "Your " + losowaCzescCiala + " looks like " + losowyPrzymiotnik + " "  + losoweSlowo;
  napis(zdanie);
