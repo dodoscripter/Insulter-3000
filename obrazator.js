f9ce648fa5a9cc5a9f381fb3ebd509c96609257d
IgorOczk <lolwatt221@gmail.com>
2016-12-31 23:29:18 +0100

    Update obrazator.js

--- obrazator.js
+++ obrazator.js
@@ -1,6 +1,6 @@
 var losoweCzesciCiala = ["Face", "Leg", "Hand", "Butt"];
 var losowePrzymiotniki = ["Stinky", "Dirty", "stupid", "ugly", "Terrible"];
-var losoweSlowa = ["fly", "Warm", "Monkey", "Lizard", "Coffe", "Mud"]
+var losoweSlowa = ["fly", "Worm", "Monkey", "Lizard", "Coffe", "Mud"]
 var losowaCzescCiala = losoweCzesciCiala[Math.floor(Math.random() * 4)];
 var losowyPrzymiotnik = losowePrzymiotniki[Math.floor(Math.random() * 5)];
 var losoweSlowo = losoweSlowa[Math.floor(Math.random() * 6)];
